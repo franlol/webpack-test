a413af735ab27b46b20df7220aea057bdc715bb5
franlol <fraaaaan@gmail.com>
2019-04-26 14:14:55 +0200

    Adding support for Jquery and Uuid. Tested OK

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,19 @@
 'use strict'
 
 import { message } from './message.js';
+import { v4 as uuid } from 'uuid';
+import $ from 'jquery';
 
 import '../styles/style.css';
 
 const main = () => {
 
     const btnAlert = document.querySelector("#btn-alert");
+    // const btnJq = document.querySelector("#btn-jquery");
+
     btnAlert.addEventListener("click", message);
+    // btnJq.addEventListener("click", () => alert(uuid()));
+    $("#btn-jquery").click(() => alert(uuid()));
 
 }
 
